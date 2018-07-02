0a78c25e74ba58f24489cd8463faa50f70184abb
perry93 <prry93@gmail.com>
2018-07-02 00:00:30 -0700

    added working local login

--- AuthApp/index.js
+++ AuthApp/index.js
@@ -1,3 +1,5 @@
+/*  EXPRESS SETUP  */
+
 const express = require('express');
 const app = express();
 
@@ -9,21 +11,69 @@ app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
 const port = process.env.PORT || 3000;
 app.listen(port , () => console.log('App listening on port ' + port));
 
-// Passport setup
 
-const passport = require("passport");
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/success', (req, res) => res.send("welcome" + req.query.username + "!"));
+app.get('/success', (req, res) => res.send("Welcome "+req.query.username+"!!"));
 app.get('/error', (req, res) => res.send("error logging in"));
 
 passport.serializeUser(function(user, cb) {
-    cb(null, user.id);
+  cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb){
-    User.findById(id, function(err, user){
-        cb(err, user);
-    });
+passport.deserializeUser(function(id, cb) {
+  User.findById(id, function(err, user) {
+    cb(err, user);
+  });
 });
+
+// mongoose setup
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/MyDatabase');
+
+const Schema = mongoose.Schema;
+const UserDetail = new Schema({
+      username: String,
+      password: String
+		});
+// Note: UserDetail(s) is not UserDetail. UserDetail is a schema
+const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
+
+
+
+// Passport setup
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+      UserDetails.findOne({
+        username: username
+      }, function(err, user) {
+        if (err) {
+          return done(err);
+        }
+
+        if (!user) {
+          return done(null, false);
+        }
+
+        if (user.password != password) {
+          return done(null, false);
+        }
+        return done(null, user);
+      });
+  }
+));
+
+// for authentication. Redirects to /success if correct or /error if wrong
+app.post('/',
+  passport.authenticate('local', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success?username='+req.user.username);
+  });
