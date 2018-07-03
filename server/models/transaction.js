35a2ec0bd4497ef02071a5ee9be2fdd8f325f0d7
perry93 <prry93@gmail.com>
2018-07-03 14:51:04 -0700

    add model junk

--- server/models/transaction.js
+++ server/models/transaction.js
@@ -1,15 +1,35 @@
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/config.js");
+module.exports = function(sequelize, DataTypes) {
+  var Transaction = sequelize.define("Transaction", {
+    title: {
+      title: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    },
+    amount: {
+      type: DataTypes.DECIMAL,
+      allowNull: false
+    },
+    time: {
+      type: DataType.NOW
+    }
+  });
 
-var Transaction = sequelize.define("transaction", {
-  type: Sequelize.STRING, //Either withdraw or deposit
-  category: Sequelize.STRING,
-  amount: Sequelize.INTEGER,
-  time: Sequelize.NOW
-});
+  Transaction.associate = function(models) {
+    // We're saying that a Post should belong to an Author
+    // A Post can't be created without an Author due to the foreign key constraint
+    Transaction.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
 
-// Syncs with DB
-Transaction.sync();
-
-module.exports = Transaction;
+  return Transaction;
+};
--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,18 @@
-var Sequelize = require("sequelize");
-// sequelize (lowercase) references my connection to the DB.
-var sequelize = require("../config/config.js");
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    // Giving the Author model a name of type STRING
+    username: DataTypes.STRING,
+    password: DataTypes.INTEGER,
+    balance: DataTypes.INTEGER
+  });
 
-var User = sequelize.define("user", {
-  username: Sequelize.STRING,
-  password: Sequelize.INTEGER,
-  balance: Sequelize.INTEGER
-});
+  User.associate = function(models) {
+    // Associating Author with Posts
+    // When an Author is deleted, also delete any associated Posts
+    User.hasMany(models.Transaction, {
+      onDelete: "cascade"
+    });
+  };
 
-// Syncs with DB
-User.sync();
-
-module.exports = User;
+  return User;
+};
