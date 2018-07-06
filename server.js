640f825a091ffd462a5bd8ced52e05f66c2fcd9f
perry93 <prry93@gmail.com>
2018-07-05 22:08:46 -0700

    updated config and models

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const bodyParser = require("body-parser");
-<<<<<<< HEAD
-// const passport = require("passport"), LocalStrategy = require('passport-local').Strategy;
-// var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const sequelize = require("sequelize");
-// const mongoose = require("mongoose");
-const routes = require("./routes");
-=======
 
->>>>>>> master
 
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
@@ -29,20 +20,18 @@ app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-<<<<<<< HEAD
-// app.use(routes);
-=======
->>>>>>> master
 
 // Routes
 
 require("./server/routes/passport.js")(app);
+// require("./server/routes/passport.js")(app);
+// require("./server/routes/passport.js")(app);
 
 // Port
 
-// db.sequelize.sync().then(function() {
+db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log(`🌎 ==> Server now on port ${PORT}!`);
   });
-// });
+});
 
--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,20 @@
-// Dependencies
-var Sequelize = require("sequelize");
-
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_library", "root", "plpl", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOSTNAME,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
-});
-
-// Exports the connection for other files to use
-module.exports = sequelize;
+}
